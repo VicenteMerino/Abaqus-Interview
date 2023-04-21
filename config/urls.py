6b7928bab4c3bd1700402ba82f0a86b407af338e
VicenteMerino <vicente.merinobadiola@gmail.com>
2023-04-21 12:41:28 -0400

    feat: add home redirect to graphs

--- config/urls.py
+++ config/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
-
+from django.urls import include, re_path, path
+from django.views.generic.base import RedirectView
 from abaqus.portfolio.views import graphs
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(("abaqus.api.urls", "api"), namespace="api")),
     path("graphs/", graphs, name="graphs"),
+    re_path(r"^.*$", RedirectView.as_view(url="/graphs/", permanent=False), name="index"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
